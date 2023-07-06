c7b72f624012c6a7ea53640e17438548fce0a3e4
Carol Casagrande <ca_casag@yahoo.com.br>
2023-07-06 16:42:04 -0300

    aula 1

---
+++ src/app/about/components/trips.tsx
@@ -0,0 +1,21 @@
+import { prisma } from '@/lib/prisma';
+import React from 'react'
+
+const getTrips = async () => {
+    const trips = await prisma.trip.findMany({});
+
+    return trips;
+}
+
+const Trips = async () => {
+    const data = await fetch('http://jsonplaceholder.typecode.com/posts').then((res) => res.json());
+    return (
+        <div>
+            {data.map((i: any) => (
+                <p key={i.id}>{i.title}</p>
+            ))}
+        </div>
+    )
+}
+
+export default Trips
